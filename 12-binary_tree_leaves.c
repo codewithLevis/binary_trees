cda940d20be262593e86b192528453abf9979a06
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-05-30 23:11:45 +0300

    Update 12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,10 +13,11 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t leaf_count = 0;
 
-	if (tree->left == NULL || tree->right == NULL)
-		return (1);
 	if (tree == NULL)
 		return (0);
+
+	if (tree->left == NULL || tree->right == NULL)
+		return (1);
 	leaf_count += binary_tree_leaves(tree->right);
 	leaf_count +=  binary_tree_leaves(tree->left);
 	return (leaf_count);
