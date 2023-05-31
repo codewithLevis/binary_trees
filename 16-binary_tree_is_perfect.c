3743039d0e226be7cf1e92c790c257de93a2a613
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-06-01 01:50:51 +0300

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -32,6 +32,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-
+	if (!tree->left && tree->right)
+		return (0);
+	if (tree->left && !tree->right)
+		return (0);
 	return (tree_height(tree->left) == tree_height(tree->right));
 }
